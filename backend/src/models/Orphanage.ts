9baeafb576cdf798d842798f907461ff3285ed3a
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2020-10-14 23:36:24 -0300

    Update Orphanage.ts

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -6,6 +6,7 @@ import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
 export default class Orphanage{
   @PrimaryGeneratedColumn('increment')
     id: number;
+
     
   @Column()
     name: string; 
